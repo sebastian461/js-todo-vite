4b8d49049de07b93280cf036fcb7728ffd40e520
sebastian461 <sebastiano-461@hotmail.com>
2023-11-09 17:53:11 -0500

    Store
    The basic storage structure was programmed

---
+++ src/store/todo.store.js
@@ -0,0 +1,21 @@
+import { Todo } from "../todos/models/todo.model";
+
+const Filters = {
+  All: "all",
+  Completed: "Completed",
+  Pending: "Pending",
+};
+
+const state = {
+  todos: [new Todo("Tarea 1"), new Todo("Tarea 2"), new Todo("Tarea 3")],
+  filter: Filters.All,
+};
+
+const initStore = () => {
+  console.log(state);
+  console.log("InitStore 🥑");
+};
+
+export default {
+  initStore,
+};
