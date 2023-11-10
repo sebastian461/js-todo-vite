c08e89cea51f2a73867721e1b058b659dfe04d9a
sebastian461 <sebastiano-461@hotmail.com>
2023-11-09 22:51:29 -0500

    Store functions
    The functionalities that the store will have have been programmed

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,28 +20,68 @@ const loadStore = () => {
   throw new Error("Not implemented");
 };
 
-const addTodo = () => {
-  throw new Error("Not implemented");
+/**
+ *
+ * @param {Filters} filter
+ */
+const getTodos = (filter = Filters.All) => {
+  switch (filter) {
+    case Filters.All:
+      return [...state.todos];
+
+    case Filters.Completed:
+      return state.filter((todo) => todo.done);
+
+    case Filters.Pending:
+      return state.filter((todo) => !todo.done);
+
+    default:
+      throw new Error(`Option ${filter} not found`);
+  }
 };
 
-const toggleTodo = () => {
-  throw new Error("Not implemented");
+/**
+ *
+ * @param {String} description
+ */
+const addTodo = (description) => {
+  if (!description) throw new Error("Description is required");
+  state.todos.push(new Todo(description));
 };
 
-const deleteTodo = () => {
-  throw new Error("Not implemented");
+/**
+ *
+ * @param {String} todoId
+ */
+const toggleTodo = (todoId) => {
+  state.todos = state.todos.map((todo) => {
+    if (todo.id === todoId) todo.done != todo.done;
+    return todo;
+  });
+};
+
+/**
+ *
+ * @param {String} todoId
+ */
+const deleteTodo = (todoId) => {
+  state.todos = state.todos.filter((todo) => todo.id !== todoId);
 };
 
 const deleteCompleted = () => {
-  throw new Error("Not implemented");
+  state.todos = state.todos.filter((todo) => todo.done);
 };
 
-const setFilter = () => {
-  throw new Error("Not implemented");
+/**
+ *
+ * @param {Filters} newFilter
+ */
+const setFilter = (newFilter = Filters.All) => {
+  state.filter = newFilter;
 };
 
 const getCurrentFilter = () => {
-  throw new Error("Not implemented");
+  return state.filter;
 };
 
 export default {
@@ -49,6 +89,7 @@ export default {
   deleteCompleted,
   deleteTodo,
   getCurrentFilter,
+  getTodos,
   initStore,
   loadStore,
   setFilter,
