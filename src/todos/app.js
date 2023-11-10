b7bcb1050210af0a34ab68cac152b116c86ccb70
sebastian461 <sebastiano-461@hotmail.com>
2023-11-10 16:05:20 -0500

    Add new todo

--- src/todos/app.js
+++ src/todos/app.js
@@ -5,6 +5,7 @@ import { renderTodos } from "./use-cases";
 
 const ElementIDs = {
   TodoList: ".todo-list",
+  NewTodo: "#new-todo-input",
 };
 
 /**
@@ -12,15 +13,30 @@ const ElementIDs = {
  * @param {String} elementId
  */
 export const App = (elementId) => {
+  //Función que dibuja las tareas en el HTML
   const displayTodos = () => {
     const todos = todoStore.getTodos(todoStore.getCurrentFilter());
     renderTodos(ElementIDs.TodoList, todos);
   };
 
+  //Función ánonima autoinvocada
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+
+  //Referencias HTML
+  const newTodo = document.querySelector(ElementIDs.NewTodo);
+
+  //Listeners
+  newTodo.addEventListener("keyup", (event) => {
+    if (event.keyCode !== 13) return; //Enter = 13
+    if (event.target.value.trim().length == 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = "";
+  });
 };
