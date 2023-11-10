94eb4d0a8540884af48d4bffa41c53fa03eab959
sebastian461 <sebastiano-461@hotmail.com>
2023-11-09 23:58:31 -0500

    Render TODO
    The way in which the to-do items are rendered was programmed

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,14 +1,26 @@
 //La sintaxis correcta para importar archivos html es agregando al final '?raw'
+import todoStore from "../store/todo.store";
 import html from "./app.html?raw";
+import { renderTodos } from "./use-cases";
+
+const ElementIDs = {
+  TodoList: ".todo-list",
+};
 
 /**
  *
  * @param {String} elementId
  */
 export const App = (elementId) => {
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(ElementIDs.TodoList, todos);
+  };
+
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
+    displayTodos();
   })();
 };
